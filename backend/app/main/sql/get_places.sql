2197f233dd77a1795cb3c9cc108eba12ce89a640
raldenprog <krylosov.andrew@gmail.com>
2021-03-27 21:09:42 +0300

    tags

--- backend/app/main/sql/get_places.sql
+++ backend/app/main/sql/get_places.sql
@@ -1,13 +1,27 @@
+with places_info as (
 select
 places.*
 , row_to_json(cities) cities
 , row_to_json(place_categories) place_categories
 , row_to_json(tourism_types) tourism_types
-, row_to_json(t) tags
 from places
 left join cities on places.city_id = cities.id
 left join place_categories on places.category_id = place_categories.id
 left join tourism_types on places.tourism_type_id = tourism_types.id
-left join place_tag pt on places.id = pt.place_id
-left join tags t on pt.tag_id = t.id
+),
+tags as (
+select
 
+places.id
+, array_agg(t) array_tags
+from places_info places
+join place_tag pt on places.id = pt.place_id
+join tags t on pt.tag_id = t.id
+group by places.id
+)
+select
+places_info.*
+, array_to_json(tags.array_tags)
+from
+places_info
+left join tags on tags.id = places_info.id
