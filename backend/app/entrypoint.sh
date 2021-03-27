7c536e45e489d05874b2ca09194678905843bf22
madwayz <djurball@list.ru>
2021-03-27 15:48:01 +0700

    * Пофиксил скелет

--- backend/app/entrypoint.sh
+++ backend/app/entrypoint.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
+while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  sleep 0.1
+done
 
 python manage.py flush --no-input
-python manage.py migrate
 python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
 
-gunicorn app.wsgi:application --bind 0.0.0.0:8000
-exec "$@"
+chmod -R 777 app/static
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --username $DJANGO_SUPERUSER_USERNAME \
+		--password $DJANGO_SUPERUSER_PASSWORD \
+        --email $DJANGO_SUPERUSER_USERNAME \
+		--skip-checks
+fi
+
+exec gunicorn app.wsgi:application --bind 0.0.0.0:8000
