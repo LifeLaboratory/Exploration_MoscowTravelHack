f21417e243ac9618aac163423c2a84ec1e3effee
raldenprog <krylosov.andrew@gmail.com>
2021-03-27 22:46:55 +0300

    reading_sort

--- backend/app/main/sql/get_places.sql
+++ backend/app/main/sql/get_places.sql
@@ -18,16 +18,22 @@ from places_info places
 join place_tag pt on places.id = pt.place_id
 join tags t on pt.tag_id = t.id
 group by places.id
-)
+),
+places_order_info as (
 select
 places_info.*
 , array_to_json(tags.array_tags) "tags"
 , google_ratings.rating google_rating
+, case when user_history.id is not null then 1 else 0 end reading_sort
 from
 places_info
 left join tags on tags.id = places_info.id
 left join google_ratings on places_info.id = google_ratings.id
+left join user_history on user_history.id_user = {id_user} and user_history.id_post = places_info.id and type = 'place'
+)
 
+select * from places_order_info
 order by
-  google_rating desc -- По оценке мировой
+    reading_sort
+  , google_rating desc -- По оценке мировой
   , created_at desc  -- Даем свежие
