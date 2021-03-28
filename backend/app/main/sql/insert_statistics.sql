b08d654a98735f10788a6577934d4dc29825c272
Anton Chusovitin <antonion2011@yandex.ru>
2021-03-28 09:01:58 +0300

    * добавил роуты для работы с избранными

--- backend/app/main/sql/insert_statistics.sql
+++ backend/app/main/sql/insert_statistics.sql
@@ -1,2 +1,2 @@
 insert into user_history("id_user", "id_post", "percent", "type")
-  values ({id_user}::int, {id_post}::int, {percent}::int, {type}::text)
+  values ({id_user}::int, {id_post}::int, {percent}::int, '{type}'::text)
